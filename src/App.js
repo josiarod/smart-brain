9a6ac8ab71f95fe3a9be9442abff0e0196382568
Josia <josiarodriguez@gmail.com>
2020-11-04 09:38:25 -0500

    Fixed port

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ class App extends Component {
      .then(response => response.json())
      .then(response => {
         if (response) {
-          fetch('https://intense-peak-26457.herokuapp.com:3000/image', {
+          fetch('https://intense-peak-26457.herokuapp.com:/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
